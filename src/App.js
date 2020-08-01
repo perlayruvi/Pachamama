239b017e28b2c972057c3598e27c65cd6a9e40b1
perlayruvi <andresfelipeorozcocano@gmail.com>
2020-08-01 00:10:50 -0500

    enlasando el proyecto react a github

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Pachamama</h1>
+    <h1>Tabla de los integrantes</h1>
+    <table>
+      <tr>
+        <td>Nombre</td>
+        <td>Nombre</td>
+        <td>Nombre</td>
+        <td>sssss</td>
+        <td>Nombre</td>
+      </tr>
+      <tr>
+      <td>Apellido</td>
+      <td>Apellido</td>
+      <td>Apellido</td>
+      <td>Apellido</td>
+      <td>Apellido</td>
+      </tr>
+    </table>
     </div>
   );
 }
